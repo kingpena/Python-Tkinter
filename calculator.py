ae4412f06260b9bc02697c97914b08d4145cd6f6
kingpena <34161102+kingpena@users.noreply.github.com>
2018-11-24 17:08:19 +0800

    Add files via upload
    updated button

--- calculator.py
+++ calculator.py
@@ -84,67 +84,67 @@ if __name__ == "__main__":
 
     # create a Buttons and place at a particular location inside the root window .
     button1 = Button(gui, text=' 1 ', fg='black', bg='#E6D72A',
-                     command=lambda: press(1), height=3, width=13)
+                     command=lambda: press(1), height=3, width=13,relief=RAISED)
     button1.grid(row=2, column=0)
 
     button2 = Button(gui, text=' 2 ',  fg='black', bg='#E6D72A',
-                     command=lambda: press(2), height=3, width=13)
+                     command=lambda: press(2), height=3, width=13,relief=RAISED)
     button2.grid(row=2, column=1)
 
     button3 = Button(gui, text=' 3 ', fg='black', bg='#E6D72A',
-                     command=lambda: press(3), height=3, width=13)
+                     command=lambda: press(3), height=3, width=13,relief=RAISED)
     button3.grid(row=2, column=2)
 
     button4 = Button(gui, text=' 4 ',  fg='black', bg='#E6D72A',
-                     command=lambda: press(4), height=3, width=13)
+                     command=lambda: press(4), height=3, width=13,relief=RAISED)
     button4.grid(row=3, column=0)
 
     button5 = Button(gui, text=' 5 ',  fg='black', bg='#E6D72A',
-                     command=lambda: press(5), height=3, width=13)
+                     command=lambda: press(5), height=3, width=13,relief=RAISED)
     button5.grid(row=3, column=1)
 
     button6 = Button(gui, text=' 6 ',  fg='black', bg='#E6D72A',
-                     command=lambda: press(6), height=3, width=13)
+                     command=lambda: press(6), height=3, width=13,relief=RAISED)
     button6.grid(row=3, column=2)
 
     button7 = Button(gui, text=' 7 ', fg='black', bg='#E6D72A',
-                     command=lambda: press(7), height=3, width=13)
+                     command=lambda: press(7), height=3, width=13,relief=RAISED)
     button7.grid(row=4, column=0)
 
     button8 = Button(gui, text=' 8 ',  fg='black', bg='#E6D72A',
-                     command=lambda: press(8), height=3, width=13)
+                     command=lambda: press(8), height=3, width=13,relief=RAISED)
     button8.grid(row=4, column=1)
 
     button9 = Button(gui, text=' 9 ', fg='black', bg='#E6D72A',
-                     command=lambda: press(9), height=3, width=13)
+                     command=lambda: press(9), height=3, width=13,relief=RAISED)
     button9.grid(row=4, column=2)
 
     button0 = Button(gui, text=' 0 ', fg='black', bg='#E6D72A',
-                     command=lambda: press(0), height=3, width=13)
+                     command=lambda: press(0), height=3, width=13,relief=RAISED)
     button0.grid(row=5, column=0)
 
     plus = Button(gui, text=' + ',  fg='black', bg='#E6D72A',
-                     command=lambda: press("+"), height=3, width=13)
+                     command=lambda: press("+"), height=3, width=13,relief=GROOVE)
     plus.grid(row=2, column=3)
 
     minus = Button(gui, text=' - ',  fg='black', bg='#E6D72A',
-                     command=lambda: press("-"), height=3, width=13)
+                     command=lambda: press("-"), height=3, width=13,relief=GROOVE)
     minus.grid(row=3, column=3)
 
     multiply = Button(gui, text=' * ',  fg='black', bg='#E6D72A',
-                     command=lambda: press("*"), height=3, width=13)
+                     command=lambda: press("*"), height=3, width=13,relief=GROOVE)
     multiply.grid(row=4, column=3)
 
     divide = Button(gui, text=' / ', fg='black', bg='#E6D72A',
-                     command=lambda: press("/"), height=3, width=13)
+                     command=lambda: press("/"), height=3, width=13,relief=GROOVE)
     divide.grid(row=5, column=3)
 
     equal = Button(gui, text=' = ', fg='black',  bg='#E6D72A',
-                   command=equalpress, height=3, width=13)
+                   command=equalpress, height=3, width=13,relief=GROOVE)
     equal.grid(row=5, column=2)
 
     clear = Button(gui, text='Clear', fg='black', bg='#E6D72A',
-                   command=clear, height=3, width=13)
+                   command=clear, height=3, width=13,relief=GROOVE)
     clear.grid(row=5, column='1')
 
 
